87233aa4a7af01bb91567151afc19ccb6aa2efe0
Jeffrey Brown <brownjs03@gmail.com>
2019-11-18 01:04:00 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -10,8 +10,8 @@ class Backer
   
   def back_project(project)
     @backed_projects << project 
-    binding.pry
-    project.backers << self
+    #binding.pry
+    project.backers << self.name
   end 
   
 end 
